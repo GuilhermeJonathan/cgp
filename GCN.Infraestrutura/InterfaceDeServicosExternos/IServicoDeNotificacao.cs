918082565fc262f2a68b538c3650f3a7d8f94a08
Guilherme Rodrigues <guilhermejonathan@hotmail.com>
2021-02-25 17:46:07 -0300

    Inclusão do serviço de notificação

---
+++ GCN.Infraestrutura/InterfaceDeServicosExternos/IServicoDeNotificacao.cs
@@ -0,0 +1,15 @@
+﻿using Campeonato.Infraestrutura.ServicosExternos.NotificacaoViaSmtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campeonato.Infraestrutura.InterfaceDeServicosExternos
+{
+    public interface IServicoDeNotificacao
+    {
+        Task EnviarNotificacao(ContatoDaNotificacao remetente, IEnumerable<ContatoDaNotificacao> destinatarios, string titulo, string mensagem);
+        //Task EnviarNotificacao(KeyValuePair<string, string> remetente, IDictionary<string, string> destinatarios, IDictionary<string, string> destinatariosOcultos, string assunto, string mensagem, string respondePara = null);
+    }
+}
---
+++ GCN.Infraestrutura/ServicosExternos/NotificacaoViaSmtp/ContatoDaNotificacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campeonato.Infraestrutura.ServicosExternos.NotificacaoViaSmtp
+{
+    public class ContatoDaNotificacao
+    {
+        public ContatoDaNotificacao(string nome, string contato)
+        {
+            Nome = nome;
+            Contato = contato;
+        }
+
+        public string Nome { get; set; }
+        public string Contato { get; set; }
+    }
+}
